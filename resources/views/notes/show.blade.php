65fc7c3507d312c47d5451541a46bccb92b23cb8
Rawda Yasser <rawdayasserg@gmail.com>
2023-06-21 20:37:26 +0300

    Feat: update delete text on the layout button to move to trash

--- resources/views/notes/show.blade.php
+++ resources/views/notes/show.blade.php
@@ -27,9 +27,8 @@
                         @csrf
                         @method('delete')
                         <button type="submit" onclick="return confirm('Are you sure you want to delete this note?')"
-                            class="px-4 py-2 ml-2 font-bold text-white bg-red-500 rounded hover:bg-red-600">Delete
-
-                            Note</button>
+                            class="px-4 py-2 ml-2 font-bold text-white bg-red-500 rounded hover:bg-red-600">
+                            Move to trash</button>
                     </form>
                 </div>
             </div>
