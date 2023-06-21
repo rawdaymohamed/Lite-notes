37f0ec7ce9bf51df04422f1c24681d10e5af7852
Rawda Yasser <rawdayasserg@gmail.com>
2023-06-21 18:30:25 +0300

    Feat: add edit note button to show note layout page

--- resources/views/notes/show.blade.php
+++ resources/views/notes/show.blade.php
@@ -1,17 +1,21 @@
 <x-app-layout>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="flex">
-                <p class="opacity-70 dark:text-white">
-                    Created at: <strong>{{ $note->created_at->diffForHumans() }}</strong>
-                </p>
-                <p class="ml-3 opacity-70 dark:text-white">
-                    Updated at: <strong>{{ $note->updated_at->diffForHumans() }}</strong>
-                </p>
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="flex justify-between">
+                <div class="flex">
+                    <p class="opacity-70 dark:text-white">
+                        Created at: <strong>{{ $note->created_at->diffForHumans() }}</strong>
+                    </p>
+                    <p class="ml-3 opacity-70 dark:text-white">
+                        Updated at: <strong>{{ $note->updated_at->diffForHumans() }}</strong>
+                    </p>
+                </div>
+                <a class="px-4 py-2 font-bold text-white bg-indigo-500 rounded hover:bg-indigo-600"
+                    href="{{ route('notes.edit', $note) }}">Edit Note</a>
             </div>
-            <div class="my-8 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
-                <h2 class="font-bold text-4xl">
+            <div class="p-6 my-8 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
+                <h2 class="text-4xl font-bold">
                     {{ $note->title }}
                 </h2>
                 <p class="mt-2 whitespace-pre-wrap"> {{ $note->text }}</p>
