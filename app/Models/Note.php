96c3d9fb58728941b9b32a87feedbcc5fd342ced
Rawda Yasser <rawdayasserg@gmail.com>
2023-06-21 20:35:02 +0300

    Fix: add softdelete to note model

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -9,6 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Note extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     protected $guarded = [];
     public function getRouteKeyName()
     {
