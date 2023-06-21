1b129561500de57791f9ed4a5d508f8d6dcf80c6
Rawda Yasser <rawdayasserg@gmail.com>
2023-06-21 20:54:36 +0300

    Fix: fix notes and trash links not showing in mobile devices

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -101,6 +101,12 @@
                 <x-responsive-nav-link :href="route('profile.edit')">
                     {{ __('Profile') }}
                 </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('notes.index')">
+                    {{ __('Notes') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('trashed.index')">
+                    {{ __('Trash') }}
+                </x-responsive-nav-link>
 
                 <!-- Authentication -->
                 <form method="POST" action="{{ route('logout') }}">
