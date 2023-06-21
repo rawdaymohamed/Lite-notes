10536087af59b39e85a346353b2653c342fde7b7
Rawda Yasser <rawdayasserg@gmail.com>
2023-06-21 20:12:23 +0300

    Feat: add user and note one to many relationship

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Note extends Model
 {
@@ -12,5 +13,9 @@ class Note extends Model
     public function getRouteKeyName()
     {
         return "uuid";
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
